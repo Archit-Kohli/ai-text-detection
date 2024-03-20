df3c4775a9b4460b4147c63e37efc8dec8240523
Archit Kohli <118905854+Archit-Kohli@users.noreply.github.com>
2024-03-20 16:01:31 +0530

    Update ai-text-detection.py

--- ai-text-detection.py
+++ ai-text-detection.py
@@ -4,7 +4,7 @@ import numpy as np
 import streamlit as st
 import pandas as pd
 
-model = tf.keras.models.load_model('ai-text-detection/model')
+model = tf.keras.models.load_model('ai-text-detection/model/')
 st.write("## AI Text Generation Detection")
 
 
